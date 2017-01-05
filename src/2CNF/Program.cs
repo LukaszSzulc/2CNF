f413ef9ee9e83749a03a35f43776ad1e80d3b462
LukaszSzulc <lukasz.schultz@gmail.com>
2017-01-05 19:52:41 +0100

    Add project files.

---
+++ src/2CNF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2CNF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
